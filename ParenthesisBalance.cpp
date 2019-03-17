ff96187c282e45ce068a80052a597685ba414a64
Mashfiq137 <38359466+Mashfiq137@users.noreply.github.com>
2019-03-18 02:17:35 +0600

    Add files via upload

---
+++ ParenthesisBalance.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool areParanthesisBalanced(string expr)
+{
+    stack<char> s;
+    char x;
+    for (int i=0; i<expr.length(); i++)
+    {
+        if (expr[i]=='('||expr[i]=='[')
+        {
+
+            s.push(expr[i]);
+            continue;
+        }
+        if (s.empty())
+           return false;
+        switch (expr[i])
+        {
+        case ')':
+            x = s.top();
+            s.pop();
+            if ( x=='[')
+                return false;
+            break;
+        case ']':
+            x = s.top();
+            s.pop();
+            if (x =='(')
+                return false;
+            break;
+        }
+    }
+    return (s.empty());
+}
+int main()
+{
+
+    int t;
+
+    cin>>t;
+
+    while(t--){
+
+    string expr ;
+
+    cin>>expr;
+
+    if (areParanthesisBalanced(expr))
+        cout << "Yes\n";
+    else
+        cout << "No\n";
+    }
+    return 0;
+}
